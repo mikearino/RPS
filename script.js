2a5e4ecda0966c4a35da5d1ae4ce98655dc6e1d8
Mike Lofgren <mikeylofgren@gmail.com>
2022-11-17 22:46:24 -0800

    write out function for returning rock paper or scissors

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('yay')
+
+const getComputerChoice = () => {
+  //randomly choose a number between one and three
+  const randomNumber = Math.floor(Math.random() * 3) + 1
+  if (randomNumber == 1) {
+    return "rock"
+  } else if (randomNumber == 2) {
+    return "paper"
+  } else {
+    return "scissors"
+  }
+}
+console.log(getComputerChoice())
