57dfc694dc72f6e9e60f4f628d1d2e36f582dc18
Mike Lofgren <mikeylofgren@gmail.com>
2022-11-20 23:48:09 -0800

    logic is sound enough for mvp

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+//initialize player scores
 let playerScore = [0]
 let compScore = [0]
 
@@ -22,28 +22,28 @@ const getComputerChoice = () => {
 const playRound = (playerChoice, compChoice) => {
   if (playerChoice == "rock" && compChoice == "scissors") {
     playerScore++
-    return "You win! Rock beats scissors!" 
+    return `You win! Rock beats scissors! Computer chose scissors`
   } if (playerChoice == "rock" && compChoice == "rock") {
-    return "Tie! Play again"
+    return `Tie! Play again. Computer chose rock`
   } if (playerChoice == "rock" && compChoice == "paper") {
     compScore++
-    return "You lose! paper beats rock!"
+    return `You lose! paper beats rock! Computer chose paper`
   } if (playerChoice == "scissors" && compChoice == "paper") {
     playerScore++
-    return "You win! Scissors beats paper!"
+    return `You win! Scissors beats paper! Computer chose paper`
   } if (playerChoice == "scissors" && compChoice == "scissors") {
-    return "Tie! Play again"
+    return `Tie! Play again. Computer chose scissors`
   } if (playerChoice == "scissors" && compChoice == "rock") {
     compScore++
-    return "You lose! rock beats scissors!"
+    return `You lose! rock beats scissors! Computer chose rock`
   } if (playerChoice == "paper" && compChoice == "rock") {
     playerScore++
-    return "You win! Scissors beats paper!" 
+    return `You win! Paper beats rock! Computer chose rock`
   } if (playerChoice == "paper" && compChoice == "paper") {
-    return "Tie! Play again"
+    return `Tie! Play again Computer chose paper`
   } if (playerChoice == "paper" && compChoice == "scissors") {
     compScore++
-    return "You lose! Scissors beats paper!" 
+    return `You lose! Scissors beats paper! Computer chose scissors`
   }
 }
 
@@ -51,20 +51,20 @@ const playRound = (playerChoice, compChoice) => {
 const game = () => {
   for (i=0; i<5; i++){
     const compChoice = getComputerChoice()
-    console.log(i)
+    console.log(`round ${i + 1}`)
     console.log(`playerScore: ${playerScore}`)
     console.log(`compScore: ${compScore}`)
     console.log(`Computer Choice: ${compChoice}`)
     const playerChoice = prompt('Choose Rock, Paper, or Scissors!')
     console.log(playRound(playerChoice,compChoice))
-    //check score first
-    if (playerScore == 5) {
-      return console.log("Congrats you win!")
-    } if (compScore == 5) {
-      return console.log("You loose booooo!")
-    } 
-
   }
+    //check score at end
+    if (playerScore > compScore) {
+      return console.log("Congrats you win!")
+    } if (compScore > playerScore) {
+      return console.log("You loose booooo!")
+    } else return console.log("It's a tie bummer!")
+
 }
 
 
