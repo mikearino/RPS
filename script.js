2ebd6ca0f427d8a923f5b4c10bfc0c3af42a1274
Mike Lofgren <mikeylofgren@gmail.com>
2022-11-20 21:34:28 -0800

    add score array/functionality

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const playerSelection = prompt("Enter player selection! Rock, Paper, or Scissors"
 )
 
+const playerScore = []
+const compScore = []
+
 const getComputerChoice = () => {
   //randomly choose a number between 1 and 3 then boolean object
   const randomNumber = Math.floor(Math.random() * 3) + 1
@@ -16,32 +19,30 @@ console.log(getComputerChoice())
   //take two params player selection/comp selection and decide who wins
 const playRound = (playerChoice, compChoice) => {
   if (playerChoice == "rock" && compChoice == "scissors") {
-    return "You win! Rock beats scissors!"
+    return "You win! Rock beats scissors!" + playerScore++
   } if (playerChoice == "rock" && compChoice == "rock") {
     return "Tie! Play again"
   } if (playerChoice == "rock" && compChoice == "paper") {
-    return "You lose! paper beats rock!"
+    return "You lose! paper beats rock!" + compScore++
   } if (playerChoice == "scissors" && compChoice == "paper") {
-    return "You win! Scissors beats paper!"
+    return "You win! Scissors beats paper!" + playerScore++
   } if (playerChoice == "scissors" && compChoice == "scissors") {
     return "Tie! Play again"
   } if (playerChoice == "scissors" && compChoice == "rock") {
-    return "You lose! rock beats scissors!"
+    return "You lose! rock beats scissors!" + compScore++
   } if (playerChoice == "paper" && compChoice == "rock") {
-    return "You win! Scissors beats paper!"
+    return "You win! Scissors beats paper!" + playerScore++
   } if (playerChoice == "paper" && compChoice == "paper") {
     return "Tie! Play again"
   } if (playerChoice == "paper" && compChoice == "scissors") {
-    return "You lose! Scissors beats paper!"
+    return "You lose! Scissors beats paper!" + compScore++
   }
 }
 
 //This starts a game with 5 rounds, records score and reports a winner or loser at the end.
 const game = () => {
-const playerScore = []
-const compScore = []
   for (i=0; i<5; i++){
-    playRound()
+    playRound(playerSelection, computerSelection)
 
   }
 }
