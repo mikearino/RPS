1fac06be2955f46082826a83444a3182c008b337
Mike Lofgren <mikeylofgren@gmail.com>
2022-11-20 22:54:23 -0800

    logic wired up into game

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const playerSelection = prompt("Enter player selection! Rock, Paper, or Scissors"
-)
 
-const playerScore = []
-const compScore = []
+
+let playerScore = [0]
+let compScore = [0]
+
 
 const getComputerChoice = () => {
   //randomly choose a number between 1 and 3 then boolean object
@@ -14,8 +14,10 @@ const getComputerChoice = () => {
   } else {
     return "scissors"
   }
+
 }
-console.log(getComputerChoice())
+
+
   //take two params player selection/comp selection and decide who wins
 const playRound = (playerChoice, compChoice) => {
   if (playerChoice == "rock" && compChoice == "scissors") {
@@ -31,7 +33,7 @@ const playRound = (playerChoice, compChoice) => {
   } if (playerChoice == "scissors" && compChoice == "rock") {
     return "You lose! rock beats scissors!" + compScore++
   } if (playerChoice == "paper" && compChoice == "rock") {
-    return "You win! Scissors beats paper!" + playerScore++
+    return "You win! Scissors beats paper!" + playerScore + 1
   } if (playerChoice == "paper" && compChoice == "paper") {
     return "Tie! Play again"
   } if (playerChoice == "paper" && compChoice == "scissors") {
@@ -42,11 +44,25 @@ const playRound = (playerChoice, compChoice) => {
 //This starts a game with 5 rounds, records score and reports a winner or loser at the end.
 const game = () => {
   for (i=0; i<5; i++){
-    playRound(playerSelection, computerSelection)
+    const compChoice = getComputerChoice()
+    console.log(i)
+    console.log(`playerScore: ${playerScore}`)
+    console.log(`compScore: ${compScore}`)
+    console.log(`Computer Choice: ${compChoice}`)
+    const playerChoice = prompt('Choose Rock, Paper, or Scissors!')
+    console.log(playRound(playerChoice,compChoice))
+    //check score first
+    if (playerScore == 5) {
+      return console.log("Congrats you win!")
+    } if (compScore == 5) {
+      return console.log("You loose booooo!")
+    } 
 
   }
 }
 
 
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+game()
+
+
