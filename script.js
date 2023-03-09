aeed048c1f95d607f0e9bfe209afb9d0697602f5
Mike Lofgren <mikeylofgren@gmail.com>
2023-03-08 23:04:23 -0800

    refactor playround logic.

--- script.js
+++ script.js
@@ -2,13 +2,16 @@
 //initialize player scores
 let playerScore = [0];
 let compScore = [0];
-let round = 0;
+let round = 1;
 
 //select the div where all the display info will go
-let display = document.querySelector('.display');
+const display = document.querySelector('.display');
 const playerDisplay = document.createElement('div');
 const compDisplay = document.createElement('div');
 const roundDisplay = document.createElement('div');
+const compChoiceDisplay = document.createElement('div');
+const playerChoiceDisplay = document.createElement('div');
+const gameDisplay = document.createElement('div');
 
 const getComputerChoice = () => {
   //randomly choose a number between 1 and 3 then boolean object
@@ -20,7 +23,6 @@ const getComputerChoice = () => {
   } else {
     return "scissors"
   }
-
 }
 
 
@@ -28,28 +30,28 @@ const getComputerChoice = () => {
 const playRound = (playerChoice, compChoice) => {
   if (playerChoice == "rock" && compChoice == "scissors") {
     playerScore++
-    return `You win! Rock beats scissors! Computer chose scissors`
+    return `You win! Rock beats scissors!`
   } if (playerChoice == "rock" && compChoice == "rock") {
-    return `Tie! Play again. Computer chose rock`
+    return `Tie! Play again.`
   } if (playerChoice == "rock" && compChoice == "paper") {
     compScore++
-    return `You lose! paper beats rock! Computer chose paper`
+    return `You lose! paper beats rock!`
   } if (playerChoice == "scissors" && compChoice == "paper") {
     playerScore++
-    return `You win! Scissors beats paper! Computer chose paper`
+    return `You win! Scissors beats paper!`
   } if (playerChoice == "scissors" && compChoice == "scissors") {
-    return `Tie! Play again. Computer chose scissors`
+    return `Tie! Play again.`
   } if (playerChoice == "scissors" && compChoice == "rock") {
     compScore++
-    return `You lose! rock beats scissors! Computer chose rock`
+    return `You lose! rock beats scissors!`
   } if (playerChoice == "paper" && compChoice == "rock") {
     playerScore++
-    return `You win! Paper beats rock! Computer chose rock`
+    return `You win! Paper beats rock!`
   } if (playerChoice == "paper" && compChoice == "paper") {
-    return `Tie! Play again Computer chose paper`
+    return `Tie! Play again.`
   } if (playerChoice == "paper" && compChoice == "scissors") {
     compScore++
-    return `You lose! Scissors beats paper! Computer chose scissors`
+    return `You lose! Scissors beats paper!`
   }
 }
 
@@ -61,22 +63,26 @@ const game = (playerChoice) => {
 
     roundDisplay.innerHTML = `round ${round}`
     display.appendChild(roundDisplay)
-    // console.log(`round ${i + 1}`)
 
     playerDisplay.innerHTML = `playerScore: ${playerScore}`
     display.appendChild(playerDisplay)
     
     compDisplay.innerHTML = `compScore: ${compScore}`
     display.appendChild(compDisplay)
+
+    compChoiceDisplay.innerHTML = `Computer Choice: ${compChoice}`
+    display.appendChild(compChoiceDisplay)
+
+    playerChoiceDisplay.innerHTML = `Player Choice: ${playerChoice}`
+    display.appendChild(playerChoiceDisplay)
+
+    gameDisplay.innerHTML = playRound(playerChoice,compChoice)
+    display.appendChild(gameDisplay)
     
-    console.log(`compScore: ${compScore}`)
     
-    console.log(`Computer Choice: ${compChoice}`)
-    // const playerChoice = prompt('Choose Rock, Paper, or Scissors!')
-    console.log(playRound(playerChoice,compChoice))
     // }
+    round++
     //check score at end
-    round ++
     if (playerScore > compScore) {
       return console.log("Congrats you win!")
     } if (compScore > playerScore) {
@@ -90,7 +96,6 @@ const game = (playerChoice) => {
 const rock = document.querySelector('.rock')
 console.log(rock)
 rock.addEventListener('click', ()=> {
-console.log('click')
 game("rock")
 })
 
